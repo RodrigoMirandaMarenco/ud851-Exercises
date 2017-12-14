45fafc5da14c7960aeb8cd7bca1d906dfcdadbdf
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2017-12-14 16:25:39 -0600

    Complete T04a.03-Exercise-PassingDataBetweenActivities

--- Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -15,11 +15,13 @@
  */
 package com.example.android.explicitintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class ChildActivity extends AppCompatActivity {
+    public static final String EXTRA_NAME = "extra_name";
 
     /* Field to store our TextView */
     private TextView mDisplayText;
@@ -32,12 +34,16 @@ public class ChildActivity extends AppCompatActivity {
         /* Typical usage of findViewById... */
         mDisplayText = (TextView) findViewById(R.id.tv_display);
 
-        // TODO (3) Use the getIntent method to store the Intent that started this Activity in a variable
+        // COMPLETE (3) Use the getIntent method to store the Intent that started this Activity in a variable
+        Intent intent = getIntent();
 
-        // TODO (4) Create an if statement to check if this Intent has the extra we passed from MainActivity
+        // COMPLETE (4) Create an if statement to check if this Intent has the extra we passed from MainActivity
+        if (intent.hasExtra(EXTRA_NAME)) {
+            String nameEntry = intent.getExtras().getString(EXTRA_NAME);
+            mDisplayText.setText(nameEntry);
+        }
+            // COMPLETE (5) If the Intent contains the correct extra, retrieve the text
 
-            // TODO (5) If the Intent contains the correct extra, retrieve the text
-
-            // TODO (6) If the Intent contains the correct extra, use it to set the TextView text
+            // COMPLETE (6) If the Intent contains the correct extra, use it to set the TextView text
     }
 }
