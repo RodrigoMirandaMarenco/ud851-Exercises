b088abb45aaad3e77e61a79539467851fbde55c2
Rodrigo Miranda <rmiranda@applaudostudios.com>
2018-01-05 14:18:50 -0600

    Complete T07.05-Exercise-AddGuests

--- Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
+++ Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
@@ -57,13 +57,22 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
         return mCursor.getCount();
     }
 
-    // TODO (15) Create a new function called swapCursor that takes the new cursor and returns void
+    // COMPLETE (15) Create a new function called swapCursor that takes the new cursor and returns void
+    public void swapCursor(Cursor cursor) {
+        if (cursor != null) {
+            if (mCursor != null) {
+                mCursor.close();
+            }
+            mCursor = cursor;
+            notifyDataSetChanged();
+        }
+    }
 
-    // TODO (16) Inside, check if the current cursor is not null, and close it if so
+    // COMPLETE (16) Inside, check if the current cursor is not null, and close it if so
 
-    // TODO (17) Update the local mCursor to be equal to  newCursor
+    // COMPLETE (17) Update the local mCursor to be equal to  newCursor
 
-    // TODO (18) Check if the newCursor is not null, and call this.notifyDataSetChanged() if so
+    // COMPLETE (18) Check if the newCursor is not null, and call this.notifyDataSetChanged() if so
 
     /**
      * Inner class to hold the views needed to display a single item in the recycler-view
